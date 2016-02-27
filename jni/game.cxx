a1769e20a08bfee17e7850c700a446feb56226bc
Konstantin Yeliseyev <kay27@bk.ru>
2016-02-28 03:46:09 +0400

    ...

--- jni/game.cxx
+++ jni/game.cxx
@@ -20,11 +20,9 @@ void Game::Init()
 void Game::Restart()
 {
   gameOver    =  0;
-  yOffs       =  0;
-  xOffs       =  0;
-  speed       =  0;
   gameStarted =  0;
-  vect        = -0.0023;
+
+  bird.Init();
 
   {
     lock_guard<mutex> lock(gapListMutex);
@@ -67,7 +65,7 @@ void Game::Render()
   float delta = (now.tv_sec - lastTime.tv_sec) * 1000000 + ((int)now.tv_usec - (int)lastTime.tv_usec);
   lastTime = now;
 
-  glClearColor(0.0, 0.0, (yOffs+1)/3.0, 1.0);
+  glClearColor(0.0, 0.0, (y+1)/3.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   bird.Render(delta);
@@ -86,17 +84,19 @@ void Game::Render()
 
 bool Game::Collision(Gap &gap)
 { // based on http://stackoverflow.com/a/1879223/5920627
+  float closestX = clamp(bird.x, gap.x - GAP_HALF_WIDTH, gap.x + GAP_HALF_WIDTH);
+  float distanceX = bird.x - closestX;
+  float dx2 = distanceX*distanceX;
 
-  // Find the closest point to the circle within the rectangle
-  float closestX = clamp(bird.xOffs, gap.x - GAP_HALF_WIDTH, gap.x + GAP_HALF_WIDTH);
-  float closestY = clamp(bird.yOffs, gap.y - GAP_HALF_HEIGHT, gap.y + GAP_HALF_HEIGHT);
+  float closestY = clamp(bird.y, 2, gap.y + GAP_HALF_HEIGHT);
+  float distanceY = bird.y - closestY;
+  if (dx2+distanceY*distanceY  <  BIRD_RADIUS*BIRD_RADIUS)
+    return true;
 
-  // Calculate the distance between the circle's center and this closest point
-  float distanceX = bird.xOffs - closestX;
-  float distanceY = bird.yOffs - closestY;
-
-  // If the distance is less than the circle's radius, an intersection occurs
-  return (distanceX * distanceX) + (distanceY * distanceY)  <  (circle.Radius * circle.Radius);
+  closestY = clamp(bird.y, gap.y - GAP_HALF_HEIGHT, -2);
+  distanceY = bird.y - closestY;
+  return
+    dx2+distanceY*distanceY  <  BIRD_RADIUS*BIRD_RADIUS;
 }
 
 inline bool Game::Clamp(float x, float a, float b)
@@ -117,38 +117,44 @@ inline void Bird::Untap()
 void Bird::Render()
 {
   if(impulse) {impulse=0; speed=-0.06;}
-  if(gameSttarted)
+  if(gameStarted)
   {
     speed+=delta/5000000;
-    if(xOffs>-0.33)
+    if(x>-0.33)
     {
-      xOffs=xOffs-delta/2000000;
-      if(xOffs<-0.34) xOffs=-0.34;
+      x=x-delta/2000000;
+      if(x<-0.34) x=-0.34;
     }
   }
-  if(!gameOver) yOffs-=delta/27000*speed;
+  if(!gameOver) y-=delta/27000*speed;
   if(!(gameStarted||gameOver))
   {
-    yOffs+=vect*delta/253427; // more: slower initial animation
-    if(yOffs>0.008) {yOffs=0.008; vect=-0.0095;}
-    if(yOffs<-0.006) {yOffs=-0.006; vect=0.011;}
+    y+=vect*delta/253427; // more: slower initial animation
+    if(y>0.008) {y=0.008; vect=-0.0095;}
+    if(y<-0.006) {y=-0.006; vect=0.011;}
   }
 
-  if(yOffs<-0.8998) if(!gameOver) { yOffs=-0.8998; gameOver=1; Toast("Game over"); }
-  if(yOffs>1.09) { yOffs=1.09; speed=0; }
+  if(y<-0.8998) if(!gameOver) { y=-0.8998; gameOver=1; Toast("Game over"); }
+  if(y>1.09) { y=1.09; speed=0; }
 
-    glUniform4f(yoffset, xOffs, yOffs, 0, 0);
-    glUniform1f(vSpeed, speed);
-//    glVertexAttribPointer(color, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex) /*stride*/, (void*)offsetof(Vertex,rgba));
+  glUniform4f(yoffset, x, y, 0.0, 0.0);
+  glUniform1f(vSpeed, speed);
 
-//    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-//    glDisableVertexAttribArray(1);
-//    glDisableVertexAttribArray(0);
-
-//    glDisableClientState(GL_VERTEX_ARRAY);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 
+}
 
+void Bird::Init(Game *_game)
+{
+  y           =  0;
+  x           =  0;
+  speed       =  0;
+  vect        = -0.0023;
+  game        = _game;
+}
+
+void Bird::Bird()
+{
+  Init(NULL);
 }

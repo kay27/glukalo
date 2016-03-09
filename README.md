193e4460fb05d38ddd747836261d6c5058f0ac1d
Konstantin Yeliseyev <kay27@bk.ru>
2016-03-09 19:16:52 +0400

    add last night changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# glukalo
+# Glukalo
+## by kay27 &copy; 2016
+> Remake of old good Flappy Bird by .GEARS
--- jni/sound.h
+++ jni/sound.h
@@ -4,6 +4,67 @@
 # include <SLES/OpenSLES.h>
 # include <SLES/OpenSLES_Android.h>
 
+  class MyAudio
+  {
+#   define BUFFER_FRAMES 441
+    public:
+      MyAudio()
+      {
+        noiseReminder = 0;
+        noiseValue = (rand() % 6000) - 3000;
+      }
+
+      void Noise(short *buffer, unsigned length, unsigned freq)
+      {
+        short *dst = buffer;
+        while(length)
+        {
+          if(noiseReminder<sampleRate) noiseReminder += freq;
+          else { noiseReminder -= sampleRate; noiseValue = (rand() % 6000) - 3000;
+          *(dst++) = noiseVal;
+          length--;
+        }
+      }
+
+    protected:
+      static short soundbuffer[BUFFER_FRAMES];
+  };
+
+static SLAndroidSimpleBufferQueueItf bqPlayerBufferQueue;
+
+// this callback handler is called every time a buffer finishes playing
+void bqPlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
+{
+    assert(bq == bqPlayerBufferQueue);
+    assert(NULL == context);
+    // for streaming playback, replace this test by logic to find and fill the next buffer
+    if (--nextCount > 0 && NULL != nextBuffer && 0 != nextSize) {
+        SLresult result;
+        // enqueue another buffer
+        result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
+        // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+        // which for this code example would indicate a programming error
+        assert(SL_RESULT_SUCCESS == result);
+        (void)result;
+    }
+}
+
+
+
+    nextCount = count;
+    if (nextSize > 0) {
+        // here we only enqueue one buffer because it is a long clip,
+        // but for streaming playback we would typically enqueue at least 2 buffers to start
+        SLresult result;
+        result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
+        if (SL_RESULT_SUCCESS != result) {
+            return JNI_FALSE;
+        }
+    }
+
+
+
+
 SLObjectItf engineObj;
 const SLInterfaceID pIDs[1] = {SL_IID_ENGINE};
 const SLboolean pIDsRequired[1]  = {SL_TRUE};

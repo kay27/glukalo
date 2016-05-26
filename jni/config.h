97c23aa4a19a28b475f1212813b4c547b08dd540
Konstantin Yeliseyev <kay27@bk.ru>
2016-05-27 00:51:48 +0400

    ...

--- jni/config.h
+++ jni/config.h
@@ -33,7 +33,7 @@
 
   const int MAX_MISSILES = 3;
 
-  const int AUTOPILOT_TIME_US = 10 * 1000 * 1000; // 10s
+  const int AUTOPILOT_TIME_US = 12 * 1000 * 1000; // 10s
 
   const float GRAVITY_TUNE = 27000;
 
